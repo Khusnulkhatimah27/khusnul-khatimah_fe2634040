3e9212bfd0f3333e8e8795ff27fab56a226d4ef4
Khusnulkhatimah27 <khusnulkhatimah27maret@gmail.com>
2022-12-15 20:57:08 +0700

    commit pertama

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "https://polite-treacle-6e0475.netlify.app" // TODO: replace this
+const studentName = "KHUSNUL KHATIMAH" // TODO: replace this
+const studentId = "FE2634040" // TODO: replace this
+
+export { url, studentName, studentId }
